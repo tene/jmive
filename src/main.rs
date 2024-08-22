f0ac46c68a630c432273df7836979489bb01d3fe
Stephen Weeks <tene@allalone.org>
2024-08-22 15:45:29 -0700

    motion

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use bevy::{
+    math::f32::Vec2,
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
 
 #[derive(Component)]
-struct Person;
-#[derive(Component)]
-struct Name(String);
-#[derive(Resource)]
-struct GreetTimer(Timer);
-pub struct HelloPlugin;
+struct Velocity(Vec2);
+pub struct LifePlugin;
 
-impl Plugin for HelloPlugin {
+impl Plugin for LifePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, setup);
-        app.add_systems(Update, (update_people, greet_people).chain());
+        app.add_systems(Update, update);
     }
 }
 
@@ -29,45 +25,30 @@ fn setup(
     let circle = Mesh2dHandle(meshes.add(Circle { radius: 50.0 }));
     let color = Color::hsl(0.0, 0.95, 0.7);
 
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: circle,
-        material: materials.add(color),
-        transform: Transform::from_xyz(
-            // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
-            0.0, 0.0, 0.0,
-        ),
-        ..default()
-    });
-
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: circle,
+            material: materials.add(color),
+            transform: Transform::from_xyz(
+                // Distribute shapes from -X_EXTENT/2 to +X_EXTENT/2.
+                0.0, 0.0, 0.0,
+            ),
+            ..default()
+        },
+        Velocity(Vec2 { x: 10.0, y: 50.0 }),
+    ));
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
+fn update(time: Res<Time>, mut query: Query<(&mut Velocity, &mut Transform)>) {
+    for (mut vel, mut trans) in &mut query {
+        trans.translation.x += vel.0.x * time.delta_seconds();
+        trans.translation.y += vel.0.y * time.delta_seconds();
     }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins(LifePlugin)
         .run();
 }
