5e88fe855d10df0dfb13b2d29959a0bb62225f10
Stephen Weeks <tene@allalone.org>
2024-08-25 19:29:02 -0700

    particle life force calculation

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,8 @@ fn setup(
     for _i in 1..1000 {
         let x = rng.gen_range(0.0..size.x) - size.x / 2.0;
         let y = rng.gen_range(0.0..size.y) - size.y / 2.0;
-        let dx = rng.gen_range(-50.0..50.0);
-        let dy = rng.gen_range(-50.0..50.0);
+        let dx = rng.gen_range(-5.0..5.0);
+        let dy = rng.gen_range(-5.0..5.0);
         let hue = rng.gen_range(0.0..360.0);
         let color = materials.add(Color::hsl(hue, 0.95, 0.7));
         commands.spawn((
@@ -52,21 +52,32 @@ fn setup(
     }
 }
 
+fn calculate_force(dist: f32, min_dist: f32, max_dist: f32, strength: f32) -> f32 {
+    if dist <= min_dist {
+        return dist / min_dist - 1.0;
+    } else if dist <= max_dist {
+        let x = (2.0 * dist - max_dist - min_dist).abs() / (max_dist - min_dist);
+        return strength * (1.0 - x);
+    } else {
+        return 0.0;
+    }
+}
+
 fn calculate_forces(time: Res<Time>, mut query: Query<(&Transform, &mut Force)>) {
     let max_distance = 100.0;
     let min_distance = 10.0;
+    let force_strength = 50.0;
     let dt = time.delta_seconds();
     let mut iter = query.iter_combinations_mut();
-    while let Some([(t1, mut f1), (t2, _f2)]) = iter.fetch_next() {
+    while let Some([(t1, mut f1), (t2, mut f2)]) = iter.fetch_next() {
         let distance = t1.translation.distance(t2.translation);
         if distance < max_distance {
-            let mut strength = dt * 100.0 / distance;
-            if distance < min_distance {
-                strength *= -1.0;
-            }
-            let dir = (t2.translation - t1.translation).normalize() * strength;
+            let strength = calculate_force(distance, min_distance, max_distance, force_strength);
+            let dir = (t2.translation - t1.translation).normalize() * strength * dt;
             f1.0.x += dir.x;
             f1.0.y += dir.y;
+            f2.0.x -= dir.x;
+            f2.0.y -= dir.y;
         }
     }
 }
